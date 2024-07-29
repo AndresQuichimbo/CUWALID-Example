874f5494fbe6f7f6e1c311c13846400d8dddc29b
Cornish_Leo <leocoote888@gmail.com>
2024-07-29 13:50:40 +0100

    Clean code

--- run_dryp.py
+++ run_dryp.py
@@ -1,13 +1,12 @@
 import time
 from cuwalid.dryp.main_DRYP import run_DRYP
-import cProfile
-import pstats
+
+
 
 if __name__ == '__main__':
-    with cProfile.Profile() as pr:
-        run_DRYP("input/dryp_input.json")
+    start = time.time()
 
-    # Write profiling stats to a text file
-    with open('profiling_stats.txt', 'w') as f:
-        stats = pstats.Stats(pr, stream=f)
-        stats.sort_stats('time').print_stats()
+    run_DRYP("input/dryp_input.json")
+    
+    end = time.time()
+    print('Time of run: %s'%(end - start))
