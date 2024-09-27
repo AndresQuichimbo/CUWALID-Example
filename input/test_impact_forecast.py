eb27665ba1c2f6c325fbf01dc58b8f6c5bbf2436
Andres Quichimbo <andresquichimbo@gmail.com>
2024-09-27 18:27:52 +0100

    update

--- input/test_impact_forecast.py
+++ input/test_impact_forecast.py
@@ -5,6 +5,6 @@ import numpy as np
 import pandas as pd
 import cuwalid.forecasting.main_impact_forecast as fcast
 
-fname = "C:/Users/Edisson/Documents/GitHub/CUWALID-Example/input/impact_forecast_input.json"
+fconfig = "C:/Users/Edisson/Documents/GitHub/CUWALID-Example/input/impact_forecast_input.json"
 
-fcast.plot_map_json(fconfig)
+fcast.plot_maps_json(fconfig)
